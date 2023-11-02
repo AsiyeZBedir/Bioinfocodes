47b2cb99b2e226729596e597bb315f259437732b
Asiye <116940286+AsiyeZBedir@users.noreply.github.com>
2023-11-02 10:42:35 +0300

    Create dna2p.py

---
+++ dna2p.py
@@ -0,0 +1,34 @@
+# Standard genetic code for translation
+genetic_code = {
+    "TTT": "F", "TTC": "F", "TTA": "L", "TTG": "L",
+    "CTT": "L", "CTC": "L", "CTA": "L", "CTG": "L",
+    "ATT": "I", "ATC": "I", "ATA": "I", "ATG": "M",
+    "GTT": "V", "GTC": "V", "GTA": "V", "GTG": "V",
+    "TCT": "S", "TCC": "S", "TCA": "S", "TCG": "S",
+    "CCT": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "ACT": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "GCT": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "TAT": "Y", "TAC": "Y", "TAA": "*", "TAG": "*",
+    "CAT": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "AAT": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+    "GAT": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+    "TGT": "C", "TGC": "C", "TGA": "*", "TGG": "W",
+    "CGT": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+    "AGT": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+    "GGT": "G", "GGC": "G", "GGA": "G", "GGG": "G"
+}
+
+def translate_dna_to_protein(dna_sequence):
+    protein_sequence = ""
+    for i in range(0, len(dna_sequence), 3):
+        codon = dna_sequence[i:i+3]
+        amino_acid = genetic_code.get(codon, "X")  # "X" for unknown codons
+        protein_sequence += amino_acid
+    return protein_sequence
+
+# Test the function
+if __name__ == "__main__":
+    dna_sequence = "ATGCGTACGTACGATGCTAGTAGCTAGTCGATGC"
+    protein_sequence = translate_dna_to_protein(dna_sequence)
+    print("DNA Sequence:", dna_sequence)
+    print("Protein Sequence:", protein_sequence)
